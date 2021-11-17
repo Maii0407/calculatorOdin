805a003e35d7c57fbd8ab404cf930575dd6af0c3
Akmal Izuddin <izuddin0407@gmail.com>
2021-11-17 12:15:54 +0800

    added a reset.css and made the basic mathematical functions, added all the buttons and basic styling of the calculator layout

---
+++ script.js
@@ -0,0 +1,19 @@
+
+//mathematical functions for the calculator
+function plus(x, y){
+    return x + y;
+}
+
+function minus(x, y){
+    return x - y;
+}
+
+function times(x, y){
+    return x * y;
+}
+
+function divide(x, y){
+    return x / y;
+}
+
+
