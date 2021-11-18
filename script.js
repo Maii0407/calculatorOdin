e2d46ecf91bda37564137eab948d6ddb734f9652
Akmal Izuddin <izuddin0407@gmail.com>
2021-11-18 21:07:43 +0800

    just did the styling for the project and making the button variables, today im pretty lazy hope tomorrows good

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 
+//global variables
+const display = document.getElementById('output');
+const numBtn = document.querySelectorAll('.number')
+const plusBtn = document.getElementById('plusBtn');
+const minusBtn = document.getElementById('minusBtn');
+const timesBtn = document.getElementById('timesBtn');
+const divideBtn = document.getElementById('divideBtn');
+const equalsBtn = document.getElementById('equalsBtn');
+const clearBtn = document.getElementById('clearBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+
 //mathematical functions for the calculator
 function plus(x, y){
     return x + y;
@@ -16,4 +27,7 @@ function divide(x, y){
     return x / y;
 }
 
+function calDisplay(userInput){
+    display.innerHTML = `${userInput}`;
+}
 
